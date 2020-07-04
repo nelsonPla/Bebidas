9964fe3f3877fef7c08faf7b090b214b37488f73
nelsonPla <65683458+nelsonPla@users.noreply.github.com>
2020-07-03 18:14:23 -0600

    guardar

---
+++ bebidas/src/context/CategoriasContext.jsx
@@ -0,0 +1,36 @@
+import React, { createContext, useState, useEffect } from 'react'
+import axios from 'axios';
+
+//Crear el context
+export const CategoriasContext = createContext();
+
+//provider es donde se encuentran las funciones y state
+const CategoriasProvider = (props) => {
+
+    //crear el state del context
+    const [ categorias, guardarCategorias ] = useState([]);
+
+    //ejecutar llamado a la API
+    useEffect(() => {
+        const obtenerCategorias = async () => {
+            const url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list';
+
+            const categorias = await axios.get(url);
+
+            guardarCategorias(categorias.data.drinks);
+        }
+        obtenerCategorias();
+    },)
+
+    return (
+        <CategoriasContext.Provider
+            value={{
+                categorias
+            }}
+        >
+            {props.children}
+        </CategoriasContext.Provider>
+    )
+}
+
+export default CategoriasProvider;
